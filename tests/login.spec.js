51b973deb94355e6b7a0fc919f2885905a139f15
christian <christian.strom92@gmail.com>
2023-10-20 18:09:34 +0200

    Changed setup file, added dotenv

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,8 +3,8 @@ const { test, expect } = require("@playwright/test")
 test("Standard user can log in", async ({ page }) => {
     await page.goto("/")
 
-    await page.fill("#user-name", "")
-    await page.fill("#password", "")
+    await page.fill("#user-name", process.env.STANDARD_USER)
+    await page.fill("#password", process.env.PASSWORD)
     await page.click("#login-button")
 
     await expect(page.locator(".title")).toHaveText("Products")
