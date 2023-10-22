6a135c9b501bcb20e6686d19bb13115521cb3716
christian <christian.strom92@gmail.com>
2023-10-22 11:30:58 +0200

    Changes to config and package.json

--- playwright.config.js
+++ playwright.config.js
@@ -2,55 +2,54 @@ const { defineConfig, devices } = require("@playwright/test")
 require("dotenv").config()
 
 module.exports = defineConfig({
-  testDir: "./tests",
-  fullyParallel: true,
-  reporter: "html",
-  use: {
-    headless: false,
-    viewport: { width: 1600, height: 900 },
-    baseURL: "https://www.saucedemo.com",
-    screenshot: "only-on-failure",
-    trace: "retain-on-failure",
-    video: "on"
-  },
-  expect: {
-    timeout: 10000
-  },
-
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+    testDir: "./tests",
+    fullyParallel: true,
+    reporter: "html",
+    use: {
+        headless: true,
+        viewport: { width: 1600, height: 900 },
+        baseURL: "https://www.saucedemo.com",
+        screenshot: "only-on-failure",
+        trace: "retain-on-failure",
+        video: "on",
+        launchOptions: {
+            slowMo: 0
+        },
     },
-
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
-
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: "Mobile Chrome",
-    //   use: { ...devices["Pixel 5"] },
-    // },
-    // {
-    //   name: "Mobile Safari",
-    //   use: { ...devices["iPhone 12"] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: "Microsoft Edge",
-    //   use: { ...devices["Desktop Edge"], channel: "msedge" },
-    // },
-    // {
-    //   name: "Google Chrome",
-    //   use: { ...devices["Desktop Chrome"], channel: "chrome" },
-    // },
-  ],
+    /*
+        projects: [
+            {
+                name: "chromium",
+                use: { ...devices["Desktop Chrome"] },
+            },
+    
+            {
+                name: "firefox",
+                use: { ...devices["Desktop Firefox"] },
+            },
+    
+            {
+                name: "webkit",
+                use: { ...devices["Desktop Safari"] },
+            },
+    
+         {
+                name: "Mobile Chrome",
+                use: { ...devices["Pixel 5"] },
+            },
+            {
+                name: "Mobile Safari",
+                use: { ...devices["iPhone 12"] },
+            },
+    
+         {
+                name: "Microsoft Edge",
+                use: { ...devices["Desktop Edge"], channel: "msedge" },
+            },
+            {
+                name: "Google Chrome",
+                use: { ...devices["Desktop Chrome"], channel: "chrome" },
+            },
+        ],
+    */
 })
